e28bd803fe123e15d6f295664dc62532a1ed5662
Rasmus Hyldgaard <rasmushyldgaard@pm.me>
2023-04-01 13:43:28 +0200

    added python solution to problem 1

---
+++ python/problem1.py
@@ -0,0 +1,19 @@
+
+
+"""
+Problem:
+    If we list all the natural numbers below 10 that are multiples of 3 or 5,
+    we get 3, 5, 6 and 9. The sum of these multiples is 23. Find the sum of
+    all the multiples of 3 or 5 below 1000.
+"""
+
+sum = 0
+
+if __name__ == "__main__":
+    
+    for n in range(1000):
+        if n % 3 == 0 or n % 5 == 0:
+            sum = sum + n
+            
+    # result
+    print(sum)
